6e7c6a047fe7a910f5b5f4594f5636f860d4cd20
matiasrv <matiasrv@live.com>
2017-04-25 00:11:25 -0300

    Primer Commit
    Finalizado

---
+++ UrlCity/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+using Newtonsoft.Json.Linq;
+
+namespace UrlCity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Ciudad());
+            Console.ReadKey();
+        }
+
+        static string Ciudad()
+        {
+            Console.Write("Ingresar un nombre de una ciudad: ");
+            string ciudad = Console.ReadLine();
+            ciudad = ciudad.ToLower();
+            try
+            {
+                WebRequest req = WebRequest.Create("https://maps.googleapis.com/maps/api/geocode/json?address=" + ciudad);
+
+                WebResponse respuesta = req.GetResponse();
+
+                Stream stream = respuesta.GetResponseStream();
+
+                StreamReader sr = new StreamReader(stream);
+
+                JObject data = JObject.Parse(sr.ReadToEnd());
+
+                string dato = "";
+                int i = -1;
+                do
+                {
+                    i++;
+                    try
+                    {
+                        dato = (string)data["results"][0]["address_components"][i]["types"][0];
+                    }
+                    catch
+                    {
+                        dato = "error";
+                    }
+                } while (dato != "country" && dato != "error");
+
+                string titulo = (string)data["results"][0]["address_components"][i]["long_name"];
+                dato = (string)data["results"][0]["address_components"][0]["long_name"];
+                if (dato.ToLower() == ciudad)
+                    return ("Pertenece a " + titulo);
+                else
+                    return "Nombre Invalido";
+            }
+            catch
+            {
+                return "Error";
+            }
+        }
+    }
+}
---
+++ UrlMovie/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+using Newtonsoft.Json.Linq;
+
+namespace UrlMovie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Pelicula());
+            Console.ReadKey();
+        }
+
+        static string Pelicula()
+        {
+            Console.Write("Ingresar un nombre de una pelicula: ");
+            string pelicula = Console.ReadLine();
+            pelicula = pelicula.ToLower();
+            try
+            {
+                WebRequest req = WebRequest.Create("http://www.omdbapi.com/?t=" + pelicula);
+
+                WebResponse respuesta = req.GetResponse();
+
+                Stream stream = respuesta.GetResponseStream();
+
+                StreamReader sr = new StreamReader(stream);
+
+                JObject data = JObject.Parse(sr.ReadToEnd());
+
+                string titulo = (string)data["Year"];
+
+                string dato = (string)data["Title"];
+
+                if (dato.ToLower() == pelicula)
+                    return ("Año de estreno: " + titulo);
+                else
+                    return "Nombre Invalido";
+            }
+            catch
+            {
+                return "Error";
+            }
+        }
+    }
+}
